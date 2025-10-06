fbb7028058bd16ecdc5ca1e0c28f069e5c962277
Professor <superelitealfa@gmail.com>
2025-10-07 00:06:14 +0200

    init + classes, package management and createTransaction

---
+++ src/models/CreateTransaction.java
@@ -0,0 +1,19 @@
+package models;
+
+import commands.CreateTransactionCommand;
+
+public class CreateTransaction {
+
+    private int amount;
+    private String description;
+
+    public CreateTransaction(){
+
+        this.amount = amount;
+        this.description = description;
+
+    }
+
+    CreateTransaction createTransaction = new CreateTransaction();
+
+}
---
+++ src/models/ViewTransaction.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.ArrayList;
+
+public class ViewTransaction {
+
+
+    public void ViewTransactionList(){
+
+
+        ArrayList<String> transactionList = new ArrayList<String>();
+
+
+    }
+
+}
